94af4a35859a73f0dd44f3a2243629b5ad71dd73
mohamadtarekk <mohamadtareksh@gmail.com>
2019-04-23 23:06:36 +0200

    test gitignore

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -8,11 +8,10 @@ public class Test {
 	public static void main(String[] args) {
         
         RedBlackTree tree = new RedBlackTree();
-        File fr = new File("dictionary.txt");
+        File fr = new File("src/resources/dictionary.txt");
         fr.readFile(tree);
         System.out.println(tree.getSize());
         System.out.println(tree.getHeight());
-
-        System.out.println(tree.getSize());
+        System.out.println(tree.getBlackHeight(tree.getRoot()));
 	}
 }
