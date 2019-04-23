46da0997a3768dae464eb61ada78fad391bf8c73
Saaalem <mahmoud_s693@yahoo.com>
2019-04-23 20:04:05 +0200

    Conflict

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -8,27 +8,10 @@ public class Test {
 	public static void main(String[] args) {
         
         RedBlackTree tree = new RedBlackTree();
-        File fr = new File("test.txt");
+        File fr = new File("dictionary.txt");
         fr.readFile(tree);
         System.out.println(tree.getSize());
         System.out.println(tree.getHeight());
-//        tree.traverseInorder(tree.getRoot());
-        System.out.println("deletion of Dragon is "+tree.delete("Dragon"));
-
-        System.out.println("deletion of book is "+tree.delete("book"));
-        System.out.println("deletion of book is "+tree.delete("book"));
-//        tree.traverseInorder(tree.getRoot());
-
-        System.out.println("insertion of book is "+tree.insert("book"));
-        System.out.println("insertion of book is "+tree.insert("book"));
-        System.out.println("insertion of book is "+tree.insert("hh"));
-//        tree.traverseInorder(tree.getRoot());
-
-        System.out.println("banana is " + tree.search("banana"));
-        System.out.println("ant is " + tree.search("aNt"));
-        System.out.println("jilly is " + tree.search("jilly"));
-        System.out.println("gilly is " + tree.search("gilly"));
-        fr.writeToFile(tree);
 
         System.out.println(tree.getSize());
 	}
